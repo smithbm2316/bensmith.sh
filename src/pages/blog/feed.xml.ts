4b5cb5f824f71bbf159db4b93ad53951c778600f
Ben Smith <bsmithdev@mailbox.org>
2022-11-10 18:32:22 -0800

    refactor: add support for <content> to RSS feed

--- src/pages/blog/feed.xml.ts
+++ src/pages/blog/feed.xml.ts
@@ -1,35 +1,72 @@
 import rss from '@astrojs/rss';
+import { z } from 'zod';
 
-interface Post {
-  url: string;
-  frontmatter: {
-    title: string;
-    pubDate?: Date;
-    draft?: boolean;
-  };
-}
+// set up a zod schema for what content we will receive from the import.meta.glob execution below
+const PostsSchema = z.array(
+  z.object({
+    url: z.string(),
+    frontmatter: z.object({
+      title: z.string(),
+      pubDate: z.string(),
+      draft: z.boolean().optional(),
+    }),
+    compiledContent: z.function().returns(z.string()),
+    rawContent: z.function().returns(z.string()),
+  }),
+);
 
-const postImportResult = import.meta.glob('./**/*.{md, mdx}', { eager: true });
-const posts = Object.values(postImportResult) as Post[];
+// import all markdown files with Vite's import.meta.glob synchronously and validate them
+const posts = PostsSchema.parse(
+  Object.values(
+    import.meta.glob('./**/*.md', {
+      eager: true,
+    }),
+  ),
+);
 
 export const get = () =>
   rss({
-    // `<title>` field in output xml
     title: 'Ben Smith\u2019s Blog',
-    // `<description>` field in output xml
     description: 'Ben\u2019s writings and thoughts about tech',
-    // base URL for RSS <item> links
     // SITE will use "site" from your project's astro.config.
     site: import.meta.env.SITE,
-    // list of `<item>`s in output xml
+    // filter out any draft posts, and then loop through each post to create our RSS feed
     items: posts
-      .filter((post) => !post.frontmatter.draft)
-      .map((post) => ({
-        link: post.url,
-        title: post.frontmatter.title,
-        pubDate: post.frontmatter.pubDate ?? new Date(),
-      })),
+      .filter((post) => !post?.frontmatter?.draft)
+      .map((post) => {
+        // we will use the values below always, even if we don't have a description for the post
+        const postData = {
+          title: post.frontmatter.title,
+          link: post.url,
+          pubDate: new Date(post.frontmatter.pubDate),
+        };
+
+        // Parse the raw content of our post to get the first 140 characters for our description
+        let description = post.rawContent().substring(0, 140);
+        if (description) {
+          // strip out any partial words by finding the last space in the string and appending a
+          // '...' to it
+          const lastSpaceIndex = description.lastIndexOf(' ');
+          description = description.substring(0, lastSpaceIndex);
+          description += '...';
+
+          return {
+            // include our common post data
+            ...postData,
+            description,
+            // add a <content> section with the HTML output from our post, and encode it properly
+            customData: `<content:encoded><![CDATA[${post.compiledContent()}]]></content:encoded>`,
+          };
+        } else {
+          // if we don't have a description, then don't generate a <content> tag either
+          return postData;
+        }
+      }),
     // (optional) inject custom xml
     customData: `<language>en-us</language>`,
-    stylesheet: '/pretty-feed-v3.xsl',
+    // inject the `xmlns:content` attribute with the namespace that defines how the
+    // <content:encoded> element should work (as it's not part of the RSS 2.0 spec by default)
+    xmlns: {
+      content: 'http://purl.org/rss/1.0/modules/content/',
+    },
   });
---
+++ src/pages/blog/jamstack-conf-2022.md
@@ -0,0 +1,8 @@
+---
+title: Jamstack Conf 2022 Takeways
+layout: ../../layouts/BlogPost.astro
+pubDate: ''
+draft: true
+---
+
+I went to Jamstack Conf 2022! It was incredible to meet people from all over the place.
