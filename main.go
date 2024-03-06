60c6014f98910f1bac86584c32aba18155e4b89d
Ben Smith <bsmithdev@mailbox.org>
2024-03-06 01:28:54 -0800

    initial commit with templ blog example from their docs

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"log"
+	"os"
+	"path"
+
+	"github.com/a-h/templ"
+	"github.com/yuin/goldmark"
+)
+
+func Unsafe(html string) templ.Component {
+	return templ.ComponentFunc(func(ctx context.Context, w io.Writer) (err error) {
+		_, err = io.WriteString(w, html)
+		return
+	})
+}
+
+func main() {
+	posts := GetPosts()
+
+	// Output path.
+	rootPath := "build"
+	if err := os.Mkdir(rootPath, 0755); err != nil {
+		log.Fatalf("failed to create output directory: %v", err)
+	}
+
+	// Create an index page.
+	name := path.Join(rootPath, "index.html")
+	f, err := os.Create(name)
+	if err != nil {
+		log.Fatalf("failed to create output file: %v", err)
+	}
+	// Write it out.
+	err = indexPage(posts).Render(context.Background(), f)
+	if err != nil {
+		log.Fatalf("failed to write index page: %v", err)
+	}
+
+	// Create a page for each post.
+	for _, post := range posts {
+		// Create the output directory.
+		dir := path.Join(rootPath, post.date.Format("2006/01/02"), post.slug)
+		if err := os.MkdirAll(dir, 0755); err != nil && err != os.ErrExist {
+			log.Fatalf("failed to create dir %q: %v", dir, err)
+		}
+
+		// Create the output file.
+		name := path.Join(dir, "index.html")
+		f, err := os.Create(name)
+		if err != nil {
+			log.Fatalf("failed to create output file: %v", err)
+		}
+
+		// Convert the markdown to HTML, and pass it to the template.
+		var buf bytes.Buffer
+		if err := goldmark.Convert([]byte(post.content), &buf); err != nil {
+			log.Fatalf("failed to convert markdown to HTML: %v", err)
+		}
+
+		// Create an unsafe component containing raw HTML.
+		content := Unsafe(buf.String())
+
+		// Use templ to render the template containing the raw HTML.
+		err = contentPage(post.slug, content).Render(context.Background(), f)
+		if err != nil {
+			log.Fatalf("failed to write output file: %v", err)
+		}
+	}
+}
