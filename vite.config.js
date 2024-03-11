b9ffc59522bf702f32be355b99503e4cfc0f9b95
Ben Smith <bsmithdev@mailbox.org>
2024-03-10 23:55:09 -0700

    simplify folder structure: site -> src, vite config into root dir

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,23 @@ import { browserslistToTargets } from "lightningcss";
 import path from "node:path";
 import { defineConfig } from "vite";
 
+/** This will be where our go app generates the static HTML to */
+const rootDir = "src";
+
 /** @type {Record<string, string>} */
 let staticHTMLFiles = {};
-for (let file of globSync("**/*.html")) {
+for (let file of globSync(`${rootDir}/**/*.html`)) {
   let pathnameRelativeToRoot = path.relative(
-    ".",
+    rootDir,
     file.slice(0, file.length - path.extname(file).length),
   );
   staticHTMLFiles[pathnameRelativeToRoot] = file;
 }
 
 export default defineConfig({
+  root: rootDir,
   appType: "mpa",
+  publicDir: "../assets",
   clearScreen: false,
   server: {
     port: 2323,
