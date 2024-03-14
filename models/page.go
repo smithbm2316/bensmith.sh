df699ee6136912b2614672ca1d39176d139bfced
Ben Smith <bsmithdev@mailbox.org>
2024-03-13 22:46:38 -0700

    add Page model for parsing markdown to a generic page

---
+++ models/page.go
@@ -0,0 +1,64 @@
+package models
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark-meta"
+	"github.com/yuin/goldmark/parser"
+)
+
+type Page struct {
+	Title   string
+	Slug    string
+	Content string
+}
+
+func NewPage(md goldmark.Markdown, metadataContext parser.Context, path string) (*Page, error) {
+	rawMarkdownBytes, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// create the slug from our markdown file's `path`
+	slugWithFileExtension, ok := strings.CutPrefix(path, Dirs.Content)
+	if !ok {
+		log.Fatalf("failed to build a slug from %s", path)
+	} else if slugWithFileExtension[:1] != "/" {
+		slugWithFileExtension = "/" + slugWithFileExtension
+	}
+	slug, ok := strings.CutSuffix(slugWithFileExtension, ".md")
+	if !ok {
+		log.Fatalf("failed to build a slug from %s", path)
+	}
+
+	// Convert the markdown to HTML, and pass it to the template.
+	var markdownBuffer bytes.Buffer
+	err = md.Convert(rawMarkdownBytes, &markdownBuffer, parser.WithContext(metadataContext))
+	if err != nil {
+		log.Fatalf("failed to convert markdown to HTML: %v", err)
+	}
+	metadata := meta.Get(metadataContext)
+
+	// Parse the title from the YAML frontmatter
+	parseErrorMsg := "Couldn't parse `%s` from " + path
+	title, ok := metadata["title"].(string)
+	if !ok {
+		log.Fatalf(parseErrorMsg, "title")
+	}
+
+	return &Page{
+		Title:   title,
+		Slug:    slug,
+		Content: markdownBuffer.String(),
+	}, err
+}
+
+// Convert the Page struct to a string representation
+func (p Page) String() string {
+	return fmt.Sprintf("%#v", p)
+}
