9b988f56f0c8388443c8dd884e8a5add3a8a116f
Ben Smith <bsmithdev@mailbox.org>
2024-09-24 16:56:19 -0700

    cleanup layouts and components for post pages

--- src/words/words.11tydata.js
+++ src/words/words.11tydata.js
@@ -13,6 +13,20 @@ function throwValidationError(field, validTypes, inputPath) {
 export default {
   layout: 'layouts/post.webc',
   tags: 'posts',
+  /** @param {string} content - pass the page's parsed content to this directly */
+  parseHeadings(content) {
+    // pool all the article headings from the page content into an array so we can create a TOC easily
+    let headings = [];
+    let matches = content.matchAll(
+      /^<h[23456].*>(?<heading>.*)<\/h[23456]>$/gm
+    );
+    for (let match of matches) {
+      if (match.groups?.heading) {
+        headings.push(match.groups.heading);
+      }
+    }
+    return headings;
+  },
   eleventyDataSchema(data) {
     // ignore the root page
     if (data.page.url === '/words/') return;
@@ -29,10 +43,10 @@ export default {
     } else if (typeof data.title !== 'string') {
       throwValidationError('title', ['string'], data.page.inputPath);
     } else if (typeof data.date !== 'string') {
-      throwValidationError('title', ['string'], data.page.inputPath);
+      throwValidationError('date', ['string'], data.page.inputPath);
     } else if (data.tags !== undefined && !Array.isArray(data.tags)) {
       throwValidationError(
-        'title',
+        'tags',
         ['string[]', 'undefined'],
         data.page.inputPath
       );
