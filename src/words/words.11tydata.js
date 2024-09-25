349359be6f65eb5590cf6a55f0821b5e7218945a
Ben Smith <bsmithdev@mailbox.org>
2024-09-24 17:41:00 -0700

    add zod validation to webc components and eleventyDataSchema invokations

--- src/words/words.11tydata.js
+++ src/words/words.11tydata.js
@@ -1,14 +1,4 @@
-/**
- * @param {string} field
- * @param {string[]} validTypes
- * @param {string} inputPath
- * @throws
- */
-function throwValidationError(field, validTypes, inputPath) {
-  throw new Error(
-    `Invalid frontmatter data for: ${inputPath}. ${field} is not ${validTypes.join(' or ')}`
-  );
-}
+import { z } from 'zod';
 
 export default {
   layout: 'layouts/post.webc',
@@ -32,23 +22,25 @@ export default {
     if (data.page.url === '/words/') return;
 
     // https://github.com/11ty/eleventy/issues/879#issuecomment-2062585031
-    if (data.draft !== undefined && typeof data.draft !== 'boolean') {
-      throwValidationError(
-        'draft',
-        ['boolean', 'undefined'],
-        data.page.inputPath
-      );
-    } else if (typeof data.layout !== 'string') {
-      throwValidationError('layout', ['string'], data.page.inputPath);
-    } else if (typeof data.title !== 'string') {
-      throwValidationError('title', ['string'], data.page.inputPath);
-    } else if (typeof data.date !== 'string') {
-      throwValidationError('date', ['string'], data.page.inputPath);
-    } else if (data.tags !== undefined && !Array.isArray(data.tags)) {
-      throwValidationError(
-        'tags',
-        ['string[]', 'undefined'],
-        data.page.inputPath
+    let result = z
+      .object({
+        draft: z.boolean().optional(),
+        layout: z.string().min(1),
+        title: z.string().min(1),
+        date: z.string().min(1),
+        tags: z.array(z.string()),
+      })
+      .safeParse(data);
+    if (!result.success) {
+      throw new Error(
+        JSON.stringify(
+          {
+            inputPath: data.page.inputPath,
+            fieldErrors: result.error.flatten().fieldErrors,
+          },
+          null,
+          2
+        )
       );
     }
   },
